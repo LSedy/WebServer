c54f4e194f390f3b949fdf2a4e7242ab455422ff
LSedy <sedylaurent@gmail.com>
2020-04-07 16:55:52 +0200

    added

---
+++ PHP/Exercices/testSite/en_tete.php
@@ -0,0 +1,16 @@
+        <header>
+                <img src="#" alt="Ceci est un logo absent, mais il reviendras, un jour prochain!">
+		
+
+		<nav class="navprincipale">
+		<ul>
+			<li><a href="#">accueil</a></li><!--whitespace-->
+			<li><a href="#">sélections</a></li><!--whitespace-->
+			<li><a href="#">archives</a></li><!--whitespace-->
+			<li><a href="#">thématiques</a></li><!--whitespace-->
+			<li><a href="#">planisphère</a></li><!--whitespace-->
+			<li><a href="#">contact</a></li><!--whitespace-->
+			<li><a href="#">liens</a></li><!--whitespace-->
+		</ul>
+		</nav>
+        </header>
---
+++ PHP/Exercices/testSite/pied_de_page.php
@@ -0,0 +1,19 @@
+    <footer id="pied_de_page">
+        <nav class="navsecondaire">
+            <ul>
+                <li><a href="#">others</a></li><!--whitespace-->
+			    <li><a href="#">relatives</a></li><!--whitespace-->
+			    <li><a href="#">commons</a></li><!--whitespace-->
+			    <li><a href="#">alpha & omega</a></li><!--whitespace-->
+			    <li><a href="#">see</a></li><!--whitespace-->
+			    <li><a href="#">sea</a></li><!--whitespace-->
+            </ul>
+        </nav>
+
+        <aside class="note">
+            <p>Les images utilisées sur ce site sont sous Licence Creative Commons -
+            </p>
+            <p>Elles sont en provenance de Flickr et de Wikipedia - Les textes sont issus Wikipedia - Copyright moi, tous droits réservés
+            </p>
+        </aside>
+    </footer>
